5f9781b6a14a97b8c031c1b2f6072ff6d3bc7eb6
MikailBusst <mikailbusst@icloud.com>
2020-04-27 09:45:13 +0800

    Added the menu and disclaimer page components along with adding the icons and fonts folders in the assets folder.

--- angularcomponents/itpproject/src/app/app.module.ts
+++ angularcomponents/itpproject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisclaimerComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
