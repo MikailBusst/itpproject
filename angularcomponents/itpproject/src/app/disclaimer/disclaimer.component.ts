1eba0cbe1628bfce0e9204821bb6dde80f0dc74c
MikailBusst <mikailbusst@icloud.com>
2020-04-27 16:50:53 +0800

    Imported the disclaimer component.

--- angularcomponents/itpproject/src/app/disclaimer/disclaimer.component.ts
+++ angularcomponents/itpproject/src/app/disclaimer/disclaimer.component.ts
@@ -10,6 +10,43 @@ export class DisclaimerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var width = window.innerWidth;
+    
+    if(width < 410){
+        $("#proceed_section").removeClass("proceed_section");
+        $("#proceed_section").addClass("proceed_section_mobile");
+    }
+    
+    if(width < 420){
+      $("#paper").removeClass("paper");
+      $("#paper").addClass("paper-mobile");
+    }
+  }
+
+  onResize(event) {
+    this.check_resize()
+}
+
+  check_resize(): void {
+    var width = window.innerWidth;
+    
+    if(width < 410){
+        $("#proceed_section").removeClass("proceed_section");
+        $("#proceed_section").addClass("proceed_section_mobile");
+    }
+    else{
+        $("#proceed_section").removeClass("proceed_section_mobile");
+        $("#proceed_section").addClass("proceed_section");
+    }
+    
+    if(width < 420){
+           $("#paper").removeClass("paper");
+           $("#paper").addClass("paper-mobile");
+    }
+    else{
+        $("#paper").removeClass("paper-mobile");
+        $("#paper").addClass("paper");
+    }
   }
 
 }
