686edbdbe5cbf1c0147168a64b31d001e453ad18
MikailBusst <mikailbusst@icloud.com>
2020-04-30 12:41:40 +0800

    Added the edit_company_registration component.

--- angularcomponents/itpproject/src/app/edit-company-registration/edit-company-registration.component.ts
+++ angularcomponents/itpproject/src/app/edit-company-registration/edit-company-registration.component.ts
@@ -10,6 +10,33 @@ export class EditCompanyRegistrationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var width = window.innerWidth;
+    
+    //document.getElementById("width").innerHTML = width;
+    
+    if(width < 601){
+        $("#proceed_section").removeClass("proceed_section");
+        $("#proceed_section").addClass("proceed_section_mobile");
+    }
   }
+
+  onResize(event) {
+    this.check_resize()
+  }
+
+  check_resize(): void {
+    var width = window.innerWidth;
+    
+    //document.getElementById("width").innerHTML = width;
+    
+     if(width < 601){
+           $("#proceed_section").removeClass("proceed_section");
+           $("#proceed_section").addClass("proceed_section_mobile");
+       }
+       else{
+           $("#proceed_section").removeClass("proceed_section_mobile");
+           $("#proceed_section").addClass("proceed_section");
+       }
+}
 
 }
