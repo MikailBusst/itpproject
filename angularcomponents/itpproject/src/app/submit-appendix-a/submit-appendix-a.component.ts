558698055d120e8649154516da82f41bad284c96
MikailBusst <mikailbusst@icloud.com>
2020-05-06 15:29:35 +0800

    Added validation to submit_appendix_a component

--- angularcomponents/itpproject/src/app/submit-appendix-a/submit-appendix-a.component.ts
+++ angularcomponents/itpproject/src/app/submit-appendix-a/submit-appendix-a.component.ts
@@ -6,103 +6,138 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-appendix-a.component.css']
 })
 export class SubmitAppendixAComponent implements OnInit {
+    undertaking_status = false
+    offer_letter_status = false
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-    var width = window.innerWidth;
-    
-    //document.getElementById("width").innerHTML = width;
-    
-    if(width < 1800){
-        $("section").removeClass("download_section_special");
-        $("section").addClass("download_section");
-    }
-    
-    if(width < 940){
-           $("input[type=file]").removeClass("file_submission");
-           $("input[type=file]").addClass("file_submission_mobile");
-       }
-    
-    if(width < 420){
-        $("section").removeClass("download_section");
-        $("section").addClass("download_section_mobile");
+    ngOnInit(): void {
+        var width = window.innerWidth;
         
-        $(".dc").removeClass("document_card");
-        $(".dc").addClass("document_card_mobile");
+        //document.getElementById("width").innerHTML = width;
         
-        $(".ct").addClass("document_card_mobile_text");
+        if(width < 1800){
+            $("section").removeClass("download_section_special");
+            $("section").addClass("download_section");
+        }
+        
+        if(width < 940){
+            $("input[type=file]").removeClass("file_submission");
+            $("input[type=file]").addClass("file_submission_mobile");
+        }
+        
+        if(width < 420){
+            $("section").removeClass("download_section");
+            $("section").addClass("download_section_mobile");
+            
+            $(".dc").removeClass("document_card");
+            $(".dc").addClass("document_card_mobile");
+            
+            $(".ct").addClass("document_card_mobile_text");
+        }
+        
+        if(width < 410){
+            $("#proceed_section").removeClass("proceed_section");
+            $("#proceed_section").addClass("proceed_section_mobile");
+        }
     }
-    
-    if(width < 410){
-        $("#proceed_section").removeClass("proceed_section");
-        $("#proceed_section").addClass("proceed_section_mobile");
-    }
-  }
 
-  onResize(event) {
-    this.check_resize()
-  }
+    onResize(event) {
+        this.check_resize()
+    }
 
-  check_resize(): void {
-    var width = window.innerWidth;
-    
-    //document.getElementById("width").innerHTML = width;
-    
-    if(width < 1800){
-        $("section").removeClass("download_section_special");
-        $("section").addClass("download_section");
-    }
-    else{
-        $("section").removeClass("download_section");
-        $("section").addClass("download_section_special");
-    }
-    
-    if(width < 940){
-        $("input[type=file]").removeClass("file_submission");
-        $("input[type=file]").addClass("file_submission_mobile");
-    }
-    else{
-        $("input[type=file]").removeClass("file_submission_mobile");
-        $("input[type=file]").addClass("file_submission");
-    }
-    
-    if(width < 464){
-        $("#submit").removeClass("submit");
-        $("#submit").addClass("submit_mobile");
-    }
-    else{
-        $("#submit").removeClass("submit_mobile");
-        $("#submit").addClass("submit");
-    }
-    
-    if(width < 420){
-        $("section").removeClass("download_section");
-        $("section").addClass("download_section_mobile_special");
+    check_resize(): void {
+        var width = window.innerWidth;
         
-        $(".dc").removeClass("document_card");
-        $(".dc").addClass("document_card_mobile");
+        //document.getElementById("width").innerHTML = width;
         
-        $(".ct").addClass("document_card_mobile_text");
-    }
-    else{
-        $("section").removeClass("download_section_mobile_special");
-        $("section").addClass("download_section");
+        if(width < 1800){
+            $("section").removeClass("download_section_special");
+            $("section").addClass("download_section");
+        }
+        else{
+            $("section").removeClass("download_section");
+            $("section").addClass("download_section_special");
+        }
         
-        $(".dc").removeClass("document_card_mobile");
-        $(".dc").addClass("document_card");
+        if(width < 940){
+            $("input[type=file]").removeClass("file_submission");
+            $("input[type=file]").addClass("file_submission_mobile");
+        }
+        else{
+            $("input[type=file]").removeClass("file_submission_mobile");
+            $("input[type=file]").addClass("file_submission");
+        }
         
-        $(".ct").removeClass("document_card_mobile_text");
+        if(width < 464){
+            $("#submit").removeClass("submit");
+            $("#submit").addClass("submit_mobile");
+        }
+        else{
+            $("#submit").removeClass("submit_mobile");
+            $("#submit").addClass("submit");
+        }
+        
+        if(width < 420){
+            $("section").removeClass("download_section");
+            $("section").addClass("download_section_mobile_special");
+            
+            $(".dc").removeClass("document_card");
+            $(".dc").addClass("document_card_mobile");
+            
+            $(".ct").addClass("document_card_mobile_text");
+        }
+        else{
+            $("section").removeClass("download_section_mobile_special");
+            $("section").addClass("download_section");
+            
+            $(".dc").removeClass("document_card_mobile");
+            $(".dc").addClass("document_card");
+            
+            $(".ct").removeClass("document_card_mobile_text");
+        }
+        
+        if(width < 410){
+            $("#proceed_section").removeClass("proceed_section");
+            $("#proceed_section").addClass("proceed_section_mobile");
+        }
+        else{
+            $("#proceed_section").removeClass("proceed_section_mobile");
+            $("#proceed_section").addClass("proceed_section");
+        }
     }
-    
-    if(width < 410){
-          $("#proceed_section").removeClass("proceed_section");
-          $("#proceed_section").addClass("proceed_section_mobile");
-    }
-      else{
-          $("#proceed_section").removeClass("proceed_section_mobile");
-          $("#proceed_section").addClass("proceed_section");
-      }
-}
 
+    display_undertaking(data): void {
+        var undertaking_file_name = data.undertaking
+        
+        document.getElementById("undertaking_file_name").innerHTML = undertaking_file_name.split("\\").pop()
+        
+        this.undertaking_status = true
+    }
+
+    display_offer_letter(data): void {
+        var offer_letter_file_name = data.offer_letter
+        
+        document.getElementById("offer_letter_file_name").innerHTML = offer_letter_file_name.split("\\").pop()
+        
+        this.offer_letter_status = true
+    }
+    
+    check_files(): void {
+        if(this.undertaking_status == false || this.offer_letter_status == false) {
+            event.preventDefault()
+            
+            if(this.undertaking_status == false) {
+                document.getElementById("undertaking_file_name").innerHTML = "<span style='color: red; font-weight: 900;'>Please upload your letter of undertaking.</span>"
+            }
+            
+            if(this.offer_letter_status == false) {
+                document.getElementById("offer_letter_file_name").innerHTML = "<span style='color: red; font-weight: 900;'>Please upload your offer letter.</span>"
+            }
+        }
+        else {
+            event.preventDefault()
+            window.location.href = "disclaimer.html"
+        }
+    }
 }
